ee1203f8f4f8363e9070270a95d09d1f9279548c
Yelim Kim <jin-yl18@mails.tsinghua.edu.cn>
2021-08-15 21:05:56 +0900

    commit123

---
+++ src/App.svelte
@@ -0,0 +1,179 @@
+<script>
+	import Footer from './Footer.svelte'
+</script>
+<header> 
+    <title>Hover Effect</title>
+   
+   	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+   
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+   <style>
+     
+ 
+
+    </style>
+ 
+</header>
+<body>
+     
+     
+        
+        <section class="team-one" id="team">
+            <img class="bg-shape-1" alt="">           
+            <div class="container">
+                <div class="title text-center">
+                    <p>Microsoft HackaLearn Korea</p>
+                    <h3>Meet Our Professional <br> Team Mentors</h3>
+                </div>  
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <div class="single">
+                            <div class="circle"></div><!-- /.team-one__circle -->
+                            <div class="inner">
+                                <h3>Justin Yoo</h3>
+                                <p>Cloud Advocate at MS</p>
+                                <div class="image">
+                                    <img src="team1.png" alt="">
+                                </div>
+                                <div class="social">
+                                    <a href="https://github.com/aliencube"><i class="fa fa-github"></i></a>
+                                    <a href="https://twitter.com/justinchronicle"><i class="fa fa-twitter"></i></a>
+                                    <a href="https://www.linkedin.com/in/justinyoo/"><i class="fa fa-linkedin"></i></a>
+                                </div> 
+                            </div> 
+                        </div><!-- /.team-one__single -->
+                    </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <div class="single">
+                            <div class="circle"></div> 
+                            <div class="inner">
+                                <h3>Youjin Jung</h3>
+                                <p>Software Engineer</p>
+                                <div class="image">
+                                    <img src="team2.png" alt="">
+                                </div> 
+                                <div class="social">
+                                    <a href="https://www.linkedin.com/in/youjinjung/"><i class="fa fa-linkedin"></i></a>
+                                    <a href="https://github.com/jung-youjin"><i class="fa fa-github"></i></a>
+                                    <a href="jungyoujin0527@gmail.com"><i class="fa fa-envelope"></i></a>
+                                </div> 
+                            </div> 
+                        </div> 
+                    </div> 
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <div class="single">
+                            <div class="circle"></div> 
+                            <div class="inner">
+                                <h3>Youjin Kim</h3>
+                                <p>Developer</p>
+                                <div class="image">
+                                    <img src="team3.png" alt="">
+                                </div><!-- /.team-one__image -->
+                                <div class="social">
+                                    <a href="https://github.com/u0jin"><i class="fa fa-github"></i></a>
+                                    <a href="https://u0jin.tistory.com/"><i class="fa fa-user"></i></a>
+                                    <a href="https://www.linkedin.com/in/youjin-kim-a73471191/"><i class="fa fa-linkedin"></i></a>
+                                </div> 
+                            </div> 
+                        </div> 
+                    </div> 
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <div class="single">
+                            <div class="circle"></div> 
+                            <div class="inner">
+                                <h3>Minho Ryang</h3>
+                                <p>Back-end Development</p>
+                                <div class="image">
+                                    <img src="team4.png" alt="">
+                                </div> 
+                                <div class="social">
+                                    <a href="https://github.com/minhoryang"><i class="fa fa-github"></i></a>
+                                    <a href="https://www.linkedin.com/in/minhoryang/"><i class="fa fa-linkedin"></i></a>
+                                    <a href="https://www.facebook.com/minhoryang/?show_switched_toast=0&show_switched_tooltip=0&show_podcast_settings=0"><i class="fa fa-facebook"></i></a>
+                                </div> 
+                            </div>
+                             
+                        </div> 
+                    </div>
+					<div class="col-lg-3 col-md-6 col-sm-12">
+                        <div class="single">
+                            <div class="circle"></div> 
+                            <div class="inner">
+                                <h3>Jay Lim</h3>
+                                <p>Front-end Development</p>
+                                <div class="image">
+                                    <img src="team5.png" alt="">
+                                </div> 
+                                <div class="social">
+                                    <a href="https://github.com/yelim421"><i class="fa fa-facebook"></i></a>
+                                    <a href="https://github.com/yelim421"><i class="fa fa-twitter"></i></a>
+                                    <a href="https://github.com/yelim421"><i class="fa fa-instagram"></i></a>
+                                </div> 
+                            </div> 
+                        </div> 
+                    </div>
+					<div class="col-lg-3 col-md-6 col-sm-12">
+                        <div class="single">
+                            <div class="circle"></div> 
+                            <div class="inner">
+                                <h3>Aimee Park</h3>
+                                <p>UX</p>
+                                <div class="image">
+                                    <img src="team6.png" alt="">
+                                </div> 
+                                <div class="social">
+                                    <a href="https://aimeepark.github.io/"><i class="fa fa-github"></i></a>
+                                    <a href="https://medium.com/@917820"><i class="fa fa-user"></i></a>
+                                    <a href="https://www.linkedin.com/in/aimeepark/"><i class="fa fa-linkedin"></i></a>
+                                </div> 
+                            </div> 
+                        </div> 
+                    </div> 
+					<div class="col-lg-3 col-md-6 col-sm-12">
+                        <div class="single">
+                            <div class="circle"></div> 
+                            <div class="inner">
+                                <h3>Sue Kim</h3>
+                                <p>Product Management/Planning</p>
+                                <div class="image">
+                                    <img src="team7.png" alt="">
+                                </div> 
+                                <div class="social">
+                                    <a href="https://github.com/yelim421"><i class="fa fa-facebook"></i></a>
+                                    <a href="https://github.com/yelim421"><i class="fa fa-twitter"></i></a>
+                                    <a href="https://github.com/yelim421"><i class="fa fa-instagram"></i></a>
+                                </div> 
+                            </div> 
+                        </div> 
+                    </div> 
+					<div class="col-lg-3 col-md-6 col-sm-12">
+                        <div class="single">
+                            <div class="circle"></div> 
+                            <div class="inner">
+                                <h3>Chulhyun Cho</h3>
+                                <p>General</p>
+                                <div class="image">
+                                    <img src="team8.png" alt="">
+                                </div> 
+                                <div class="social">
+
+                                    <a href="https://twitter.com/ilovehojin"><i class="fa fa-twitter"></i></a>
+                                    <a href="https://www.linkedin.com/in/chulhyun-cho-3aab7755/"><i class="fa fa-instagram"></i></a>
+                                </div> 
+                            </div> 
+                        </div> 
+                    </div>   
+                </div> 
+            </div> 
+        </section> 
+     
+        
+ 
+ 
+   
+   
+</body>
+
+<Footer />
